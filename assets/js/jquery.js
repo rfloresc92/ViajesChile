b82863bbdb747fa246bcd1720be20caa2c84ca34
rubenflores <rfloresc92@gmail.com>
2024-02-01 11:49:14 -0300

    arreglos jquery

---
+++ assets/js/jquery.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    $('#next').tooltip({
+        title: 'Siguiente imagen',
+        placement: 'auto'
+    });
+
+    $('#prev').tooltip({
+        title: 'Imagen anterior',
+        placement: 'auto'
+    });
+
+    $('#desid').on('click', function () {
+        $('#dessec').toggle();
+    });
+});
