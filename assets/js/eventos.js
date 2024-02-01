fec4bd3904fd2eed7a111feffaa5890a0fed0357
rubenflores <rfloresc92@gmail.com>
2024-02-01 20:07:04 -0300

    Se agregan comentarios a las funciones JS

--- assets/js/eventos.js
+++ assets/js/eventos.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function () {
 
+
+    //Smooth Scroll
     $("a").on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
@@ -13,24 +15,29 @@ $(document).ready(function () {
         }
     });
 
+    //Tooltip para boton Siguiente en carousel
     $('#next').tooltip({
         title: 'Siguiente imagen',
         placement: 'auto'
     });
 
+    //Tooltip para boton Anterior en carousel
     $('#prev').tooltip({
         title: 'Imagen anterior',
         placement: 'auto'
     });
 
+    //Desaparece seccion destacados al hacer click en titulo (DESTACADOS)
     $('#desid').on('click', function () {
         $('#dessec').toggle();
     });
 
+    //Modal para boton Enviar en seccion Contacto
     $('#enviar').on('click', function(){
         $('#miModal').modal('show');
     });
 
+    //Funcion ocultar modal al boton Aceptar del Modal
     $('#aceptar').on('click', function(){
         $('#miModal').modal('hide');
     });
